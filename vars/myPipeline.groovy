cbffc949efa43f9d2df932af7cdb940ce0f270d8
naresh26git <111833906+naresh26git@users.noreply.github.com>
2023-12-16 18:19:46 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -5,7 +5,7 @@ def call() {
             stage('Git Checkout') {
                 steps {
                     script {
-                        git branch: 'main', credentialsId: 'your-git-credentials', url: 'https://github.com/naresh26git/multi-branch.git'
+                        git branch: 'node-dev', url: 'https://github.com/naresh26git/multi-branch.git'
                     }
                 }
             }
@@ -16,16 +16,16 @@ def call() {
                     }
                 }
             }
-            stage ('SonarQube Analysis') {
-                steps {
-                    script { 
-                        def scannerHome = tool 'sonarscanner4'
-                        withSonarQubeEnv('sonar-pro') {
-                            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=node.js-app"
-                        }
-                    }
-                }
-            }
+            // stage ('SonarQube Analysis') {
+            //     steps {
+            //         script { 
+            //             def scannerHome = tool 'sonarscanner4'
+            //             withSonarQubeEnv('sonar-pro') {
+            //                 sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=node.js-app"
+            //             }
+            //         }
+            //     }
+            // }
             stage('Docker Build Images') {
                 steps {
                     script {
@@ -34,27 +34,27 @@ def call() {
                     }
                 }
             }
-            stage('Docker Push') {
-                steps {
-                    script {
-                        withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
-                            sh "docker login -u naresh2603 -p ${dockerPassword}"
-                            sh 'docker push naresh2603/multi:v2'
-                            sh 'docker rmi naresh2603/multi:v2'
-                        }
-                    }
-                }
-            }
-            stage('Deploy on k8s') {
-                steps {
-                    script {
-                        withKubeCredentials(kubectlCredentials: [[ credentialsId: 'kubernetes', namespace: 'ms' ]]) {
-                            sh 'kubectl apply -f kube.yaml'
-                            sh 'kubectl get pods -o wide'
-                        }
-                    }
-                }
-            }
+            // stage('Docker Push') {
+            //     steps {
+            //         script {
+            //             withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
+            //                 sh "docker login -u naresh2603 -p ${dockerPassword}"
+            //                 sh 'docker push naresh2603/multi:v2'
+            //                 sh 'docker rmi naresh2603/multi:v2'
+            //             }
+            //         }
+            //     }
+            // }
+            // stage('Deploy on k8s') {
+            //     steps {
+            //         script {
+            //             withKubeCredentials(kubectlCredentials: [[ credentialsId: 'kubernetes', namespace: 'ms' ]]) {
+            //                 sh 'kubectl apply -f kube.yaml'
+            //                 sh 'kubectl get pods -o wide'
+            //             }
+            //         }
+            //     }
+            // }
         }
     }
 }
