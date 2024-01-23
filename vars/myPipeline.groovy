adf8c5c2a90a9f2a063635d7d719dd00b967ecc6
naresh26git <111833906+naresh26git@users.noreply.github.com>
2024-01-23 13:28:06 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -29,8 +29,9 @@ def call() {
             stage('Docker Build Images') {
                 steps {
                     script {
-                        sh 'docker build -t naresh2603/multi:v2 .'
+                        sh 'docker build -t naresh2603/multi:v3 .'
                         sh 'docker images'
+                        sh 'docker ps'
                     }
                 }
             }
